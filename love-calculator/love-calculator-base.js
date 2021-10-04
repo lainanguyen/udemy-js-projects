a24cd6c647aa633b1409a832ab770a69e9e810d3
Laina Nguyen <78044530+lainanguyen@users.noreply.github.com>
2021-10-04 10:06:38 -0400

    Create love-calculator-base.js

---
+++ love-calculator/love-calculator-base.js
@@ -0,0 +1,7 @@
+var firstName = prompt("Enter first name");
+var secondName = prompt("Enter second name");
+
+var n = Math.random();
+n = Math.floor(n*=100)+1;
+
+alert( firstName + " and " + secondName + " are " + n + "% compatible!");
